773d72968c9ee8a625151a9d4f8ea0546aed5a65
koppy333-333-333 <97503569+koppy333-333-333@users.noreply.github.com>
2022-03-09 09:54:52 -0300

    Add files via upload

---
+++ Tool-Hell.py
@@ -0,0 +1,650 @@
+import  os 
+import requests
+from time import sleep
+import  json
+
+
+
+
+ip = requests.get('https://ipwhois.app/json/')
+
+resultado = ip.json()
+
+while True:
+	os.system('clear')
+	print(f'''\033[1;34m>CARREGANDO...<	\033[1;33m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;32m>CARREGANDO...<	\033[1;35m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;92m>CARREGANDO...<	\033[1;36m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;92m>CARREGANDO...<\033[1;93m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;97m>CARREGANDO...<
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	sleep(1)
+	os.system('clear')
+	break
+while True:
+	os.system('clear')
+	
+	
+	print (f"""\033[32m 
+ _____ ___   ___  _       _   _ _____ _     _
+|_   _/ _ \ / _ \| |     | | | | ____| |   | |
+  | || | | | | | | |     | |_| |  _| | |   | |
+  | || |_| | |_| | |___  |  _  | |___| |___| |___
+  |_| \___/ \___/|_____| |_| |_|_____|_____|_____|\033[34m
+your ip > \033[1;36m{resultado['ip'] }\033[0;0m
+\033[31mcoded by koppy 404
+__________________
+ig: koppy999_\033
+\033[1m \033[36m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$  \033[31m  * \033[32m `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$\033[31m  * \033[32m   /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~""")
+	
+
+
+
+
+
+	amarelo = '\033[33m'
+	verde = '\033[32m'
+	vermelho = '\033[31m'
+
+
+	print (f'''
+ 
+{amarelo}[ 2 ] {verde}Zphisher
+
+{amarelo}[ 3 ] {verde}sql map
+
+{amarelo}[ 4 ] {verde}torshammer
+
+{amarelo}[ 5 ] {verde}Urlspoof
+
+{amarelo}[ 6 ] {verde}fbi
+
+{amarelo}[ 7 ] {verde}seu ip
+
+{amarelo}[ 8 ] {verde}seeker | engenharia social 
+
+{amarelo}[ 9 ] {verde}RapidPayload | Criação de payload de uma forma mais rápida
+
+{amarelo}[10 ] {verde}Hack WiFi | sobre invasão de redes WiFi
+
+{amarelo}[11 ] {verde}CURSO DA DESEC SECURITY - NOVO PENTEST PROFISSIONAL
+ 
+{amarelo}[12 ] {verde}PACK DE CURSOS, FORENSE, PROGRAMAÇÃO, HACKER, EBOOK'S ETC..
+
+{amarelo}[13 ] {verde}CURSO DA SOLYD DE PYTHON PARA PENTEST NA PRÁTICA
+
+{amarelo}[14 ] {vermelho} suicídio | opção para travar o sistema do seu celular.
+
+''')
+
+
+	escolham = input('\033[34m DIGITE A OPÇÃO: ')
+	
+	if escolham == '1':
+		os.system('clear')
+		os.system('figlet TOOL HELL')
+		print(f'''
+
+ 
+   ''')
+		
+		
+		
+			
+	elif escolham == '2':
+		os.system('clear')
+		os.system('figlet TOOL HELL')
+			
+		print (f'''
+			
+{amarelo}[ 1 ] {verde} instalar ferramenta
+
+{amarelo}[ 2 ] {verde} qual é a função do Zphisher ?
+			''')
+		zphiser = input('DIGITE A OPÇÃO : ')
+		
+		if zphiser == '1':
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			
+			print(f'{amarelo} INICIANDO A INSTALAÇÃO..')
+			
+			os.system("apt update -y")
+			os.system("pkg upgrade -y") 
+			os.system("pkg install git -h")
+			os.system("pkg install php -y")
+			os.system("git clone  git://github.com/htr-tech/zphisher.git")
+			os.system("mv zphisher /data/data/com.termux/files/home")
+			print(f'{verde} INSTALAÇÃO CONCLUÍDA! VOLTANDO AO MENU PRINCIPAL...')
+			sleep(5)
+			
+			
+			
+		elif zphiser == '2':
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			print(f'''{verde} Uma ferramenta de phishing automatizada com mais de 30 modelos. Esta ferramenta é feita apenas para fins educacionais! O autor não será responsável por qualquer uso indevido deste kit de ferramentas!
+			
+			''')
+			
+			zphiser1 = input('APERTE ENTER PARA VOLTAR O MENU')
+			
+			os.system('clear')
+		else:
+			os.system('clear')
+			
+			print('OPÇÃO INVALIDA VOLTANDO AO MENU ,INICIAL')
+			
+			sleep(4)
+		
+
+	elif escolham == '3':
+		os.system('clear')
+		os.system('figlet TOOL HELL')
+		
+		
+		
+		print(f'''
+		
+{amarelo}[ 1 ]{verde} instalar ferramenta
+{amarelo}[ 2 ]{verde} qual é a função do SQL MAP ?''')
+		sql = input('DIGITE A OPÇÃO ')
+		
+		if sql == '1':
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			os . system ( "apt update -y" )
+			os . system ( "pkg upgrade -y" )
+			os . system ( "pkg install git -y" )
+			os . system ( "git clone https://github.com/sqlmapproject/sqlmap" )
+			os . system ( "mv sqlmap /data/data/com.termux/files/home" )
+			
+		elif sql == '2':
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			
+			print (''' \033[34m sqlmap é uma ferramenta de teste de penetração de código aberto que automatiza o processo de detecção e exploração de falhas de injeção de SQL e controle de servidores de banco de dados. Ele vem com um poderoso mecanismo de detecção, muitos recursos de nicho para o melhor testador de penetração e uma ampla gama de opções, incluindo impressão digital de banco de dados, busca de dados do banco de dados, acesso ao sistema de arquivos subjacente e execução de comandos no sistema operacional via out- conexões de banda. 
+			
+			''')
+			
+	
+			sql1 = input('APERTE ENTER PARA VOLTAR AO MENU PRINCIPAL')
+			
+	elif escolham == '4':
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			
+			print(f'''
+{amarelo}[ 1 ] {verde}baixar ferramenta
+{amarelo}[ 2 ] {verde}qual é a função do torshammer ? 
+			
+			''')
+			torshammer = input(f'DIGITE A OPÇÃO : {amarelo}')
+			
+			if torshammer == '1':
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				os.system ( "apt update -y && pkg upgrade -y" )
+				os.system ( "pkg install python -y" )
+				os.system ( "pkg install python2 -y" )
+				os.system ( "pkg install git -y" )
+				os.system("git clone https://github.com/dotfighter/torshammer")
+				os.system("mv torshammer /data/data/com.termux/files/home")
+				print(' INSTALAÇÃO CONCLUÍDA!')
+				
+				
+				
+				
+			elif torshammer == '2':
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				
+				print('''
+				
+				
+				FERRAMENTA DE ATAQUES DE WEB SITES.
+				''')
+				
+				input ('ENTER PARA VOLTA O MENU')
+			
+			
+			else :
+				os.system('clear')
+				print ('OPÇÃO INVÁLIDA. VOLTANDO AO MENU PRINCIPAL ')
+				
+				
+	elif escolham == '5':
+				print ('\033[36m')
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				
+				print (f'''
+				
+{amarelo}[ 1 ] {verde}baixar ferramenta
+{amarelo}[ 2 ] {verde}qual é a função do Urlspoof
+
+''')
+				url = input('\033[36m DIGITE A OPÇÃO: ')
+				
+				if url == '1':
+					print ('\033[36m')
+					os.system('clear')
+					os.system('figlet TOOL HELL')
+					os.system ( "apt update -y" )
+					os.system ( "pkg upgrade -y" )
+					os.system ( "pkg install python -y" )
+					os . system ( "pkg install git -y" )
+					os.system ( "git clone https://github.com/Darkmux/URLSpoof" )
+					os.system ( "mv URLSpoof /data/data/com.termux/files/home" )
+					
+					
+				elif url == '2':
+					print ('\033[36m')
+					os.system('clear')
+					os.system('figlet TOOL HELL')
+					
+					print ('''
+					
+O URLSpoof é uma herramienta desarrollada para cortar URL(s) e agregar palavras (de nossa seleção) a esse(s) URL(s) usando um poco de engenharia social, de esta forma, se obter um enlace cortado, camuflado e suplantado a identidade de cualquier otro enlace.
+					''')
+					
+					input ('ENTER PARA VOLTAR AO MENU PRINCIPAL')
+	
+	
+	elif escolham == '6':
+			print ('\033[36m')
+			os.system('clear')
+			os.system('figlet TOOL HELL')
+			
+			print(f'''
+			
+{amarelo}[ 1 ] {verde}instalar a ferramenta
+{amarelo}[ 2 ] {verde}qual é a função do fbi
+			
+					''')
+					
+			fbi = input('DIGITE A OPÇÃO: \033[34m')
+			
+			if fbi == '1':
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				os.system("apt update -y")
+				os.system("cd ..")
+				os.system("pkg upgrade -y")
+				os.system("pkg install python -y")
+				os.system("pkg install python2 -y")
+				os.system("pkg install git -y")
+				os.system("git clone https://github.com/xHak9x/fbi.git")
+				os.system("mv fbi /data/data/com.termux/files/home")
+				
+				
+				print('instalação completa')
+				
+				sleep(3)
+				
+				
+				
+			elif fbi == '2':
+				print('\033[1;35m')
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				
+				print('''
+				
+FBI é uma coleta precisa de informações de contas do Facebook, todas as informações confidenciais podem ser facilmente coletadas, mesmo que o alvo converta toda a sua privacidade para (somente eu), informações confidenciais sobre residência, data de nascimento, ocupação, número de telefone e endereço de e-mail.
+				''')
+				
+				input('ENTER PARA VOLTAR AO MENU PRINCIPAL')
+				
+			
+			else:
+				
+				print('\033[1;35m')
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				
+				print(' OPÇÃO INVÁLIDA VOLTANDO AO MENU PRINCIPAL..')
+				
+				sleep(5)
+				
+				
+	elif escolham == '7':
+				os.system('clear')
+				print(f'''\033[36m YOUR IP² > \033[1;36m{resultado['ip'] }\033[0;0m''')
+				print(f'\033[1m{amarelo}ip do seu roteador{verde}')
+				os.system("ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'")
+				
+				
+				
+				
+				input('''
+ENTER PARA VOLTA O MENU PRINCIPAL..''')
+				
+				
+				
+				
+	elif escolham == '8':
+				os.system('clear')
+				os.system('figlet TOOL HELL')
+				print(f'''
+				
+{amarelo}[ 1 ] {verde}instalar ferramenta
+{amarelo}[ 2 ] {verde}qual é a função do seeker ?
+			
+					''')
+					
+				seeker = input('\033[36m DIGITE A OPÇÃO : ')
+					
+				if seeker == '1':
+						os.system('clear')
+						os.system('figlet TOOL HELL')
+						os.system('git clone https://github.com/thewhiteh4t/seeker.git')
+						os.system('mv  seeker/ /data/data/com.termux/files/home ')
+						os.system('pip3 install requests')
+						os.system('apt install python3 python3-pip php')
+						
+						
+						
+				elif seeker == '2':
+					os.system('clear')
+					print(f'{amarelo}')
+					os.system('clear')
+					os.system('figlet TOOL HELL')
+					
+					print('''
+					
+Localize com precisão smartphones usando engenharia social , etc ...
+					
+					''')
+					input('DIGITE ENTER PARA VOLTA AO MENU PRINCIPAL')
+						
+						
+						
+						
+						
+						
+				else :
+						os.system('clear')
+						print('OPÇÃO INVÁLIDA VOLTANDO AO MENU PRINCIPAL ...')
+						
+						sleep(5)
+						
+
+	elif escolham == '9':
+					os.system('clear')
+					os.system('figlet TOOL HELL')
+					print(f'''
+					
+{amarelo}[ 1 ] {verde}instalar ferramenta
+{amarelo}[ 2 ] {verde}qual é a função do RapidPayload?					
+	''')
+					
+					
+					RapidPayload = input('DIGITE A OPÇÃO : ')
+					
+					if RapidPayload == '1':
+						os.system('clear')
+						os.system('figlet TOOL HELL')
+						os.system('git clone https://github.com/AngelSecurityTeam/RapidPayload')
+						os.system('mv RapidPayload /data/data/com.termux/files/home ')
+						print('INSTALAÇÃO CONCLUÍDA ')
+						
+						
+						
+					elif RapidPayload == '2':
+						os.system('clear')
+						print('\033[1m \033[36')
+						os.system('figlet TOOL HELL')
+						print('''
+Framework RapidPayload - Gerador de carga útil Metasploit | Invasão do antivírus Crypter FUD						
+''')
+						
+						
+						input(' ENTER PARA VOLTA O MENU PRINCIPAL ')
+						
+						
+						
+					else:
+						print('''
+
+OPÇÃO INVÁLIDA VOLTANDO AO MENU PRINCIPAL..
+						
+							''')
+						sleep(4)
+						
+	elif escolham == '10':
+					os.system('clear')
+					print('\033[1m \033[31')
+					os.system('figlet TOOL HELL')
+					
+					print(f'''
+					
+{amarelo}[ 1 ] {verde}instalar o aplicativo
+{amarelo}[ 2 ] {verde}como funciona o aplicativo
+					''')
+					
+					app = input('DIGITE A OPÇÃO: ')
+					
+					
+					if app == '1':
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet TOOL HELL')
+						os.system('termux-open-url https://seulink.online/vqC3')
+						
+						
+					
+						
+						
+						
+					elif app == '2':
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet TOOL HELL')
+						os.system('termux-open-url https://seulink.online/I22UP8U ')
+						
+						
+					
+						
+						
+						
+					else:
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet TOOL HELL')
+					
+						
+						
+						print('''OPÇÃO INVÁLIDA VOLTANDO AO MENU PRINCIPAL ...''')
+						
+						sleep(4)
+						
+	elif escolham == '11':
+							os.system('termux-open-url https://drive.google.com/drive/folders/1Ce9lXBn9ldxZUeljY1BhCQ12VTlfcjUm ')
+				
+				
+	elif escolham == '12':
+							os.system('termux-open-url https://drive.google.com/drive/folders/1IASHHkXtf0lvEOi0Z6Jl8nIqSWHWtEkF ')
+							
+							
+	elif escolham == '13':
+					os.system('termux-open-url https://drive.google.com/drive/folders/1YQ4FxlWw1DnYCE8d5Idx-8gOEqwCZzs7 ')
+					
+					
+	elif escolham == '14':
+					os.system('clear')
+					print('\033[1m \033[31')
+					os.system('figlet TOOL HELL')
+					os.system('pkg install bash -y')
+					print(f'''
+{amarelo}[ 1 ] {verde} INICIAR O VÍRUS 	
+					''')
+					
+					
+					vírus = input ('DIGITE A OPÇÃO: ')
+					
+					if  vírus == '1':
+						while True:
+							print('Se suicidar')
+							os.system(':(){ :|:& };:')
+						
+						
+	else:
+	   		os.system('clear')
+	   		print('OPÇÃO INVÁLIDA VOLTANDO AO MENU ...')
+	   		sleep(3)
+	
+					
+					
+
+					
+					
+					
+				
+				
+		
+				
+		
+		
+	
+				
+				
+
+			
+			
+		
